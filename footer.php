496542226348c7d247abf2f896dbc54be2d5503f
louiski10 <82381363+louiski10@users.noreply.github.com>
2021-08-02 14:55:12 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="footer.css">
+</head>
+<body>
+    <footer class="page-footer">
+        <div class="aboutus footer-section">
+                <div class="test">
+                    <h2 class="section-header">About us</h2>
+                </div>
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptates deleniti sequi ut provident repellat! Cum ad, unde corrupti provident deleniti facilis officia esse, sed adipisci fuga animi ducimus, impedit itaque sit.</p>
+                 <ul class="sci">
+                    <li><a href="#"><i class="fa fa-facebook-square" style="font-size:36px"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter-square" style="font-size:36px"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram" style="font-size:36px"></i></a></li>
+                    <li><a href="#"><i class="fa fa-youtube-square" style="font-size:36px"></i></a></li>
+                </ul>
+        </div>
+        <div class="quickLinks footer-section">
+            <div class="test">
+                <h2 class="section-header">Quick Links</h2>
+            </div>
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Terms & conditions</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="contact footer-section">
+            <div class="test">
+                <h2 class="section-header">Contact Info</h2>
+            </div>
+            <ul class="info">
+                <li>
+                    <span><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                    <span>647 Linda Street <br>
+                    Phoenixville, PA 19460, <br>USA</span>
+                </li>
+                <li>
+                    <span><i class="fa fa-phone" aria-hidden="true"></i></span>
+                    <p><a href="tel:123456789">+1 23 45 67 89</a><br>
+                       <a href="tel:123456789">+1 23 45 67 89</a></p>
+                </li>
+                <li>
+                    <span><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                    <p><a href="mailto:knowmore@email.com">mailto:knowmore@email.com</a></p>
+                </li>
+        </div>
+
+    </footer>
+</body>
+</html>
